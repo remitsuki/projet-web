d9d19d51e2f3c3f723805db37ee36db793a7099c
Sats12 <85562769+Sats12@users.noreply.github.com>
2021-06-19 19:58:38 +0200

    Update index.php

--- Shop/index.php
+++ Shop/index.php
@@ -8,17 +8,13 @@ if (isset($_SESSION['user'])) {
 }
 if (isset($_SESSION['error'])) {
   echo "
-    <div class='callout callout-danger text-center'>
       <p>" . $_SESSION['error'] . "</p> 
-    </div>
   ";
   unset($_SESSION['error']);
 }
 if (isset($_SESSION['success'])) {
   echo "
-    <div class='callout callout-success text-center'>
       <p>" . $_SESSION['success'] . "</p> 
-    </div>
   ";
   unset($_SESSION['success']);
 }
