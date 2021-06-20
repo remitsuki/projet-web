330a5ea9809dd5b8ebc12e5bd2bc6fe734ea20b6
Felix-ROCH <85563480+Felix-ROCH@users.noreply.github.com>
2021-06-20 11:12:07 +0200

    Update index.php

--- Administration/index.php
+++ Administration/index.php
@@ -50,7 +50,7 @@ $nombredepages = 1;
           <img src="https://lipn.univ-paris13.fr/~guerif/images/guerif_small.png" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24">
           <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
           <circle cx="12" cy="13" r="4" /></img>
-          <strong>Sauce Site : Page d'administration</strong>
+          <strong>Sauce Site : Page d'Administration</strong>
           <a href="ajouter.php" class="btn btn-sm btn-outline-secondary">ajouter un produit</a>
         </a>
         <a href="deconnection.php">déconnection</a>
