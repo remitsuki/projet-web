5a20dd59d9f52d55d4401ee059bb8dad434ea9e6
Abel BECUWE <54815174+UnCureDent@users.noreply.github.com>
2021-06-20 17:43:28 +0200

    Add files via upload

--- Panier/payement.php
+++ Panier/payement.php
@@ -1,67 +1,60 @@
-
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="moi">
-    <meta name="generator" content=" 0.83.1">
-    <title>Checkout example · Bootstrap v5.0</title>
+    <title>Payement accepté</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
-
-
-
-    <!-- Bootstrap core CSS -->
-    <!-- <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">-->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-     <style>
-         .bd-placeholder-img {
-             font-size: 1.125rem;
-             text-anchor: middle;
-             -webkit-user-select: none;
-             -moz-user-select: none;
-             user-select: none;
-         }
-
-         @media (min-width: 768px) {
-             .bd-placeholder-img-lg {
-                 font-size: 3.5rem;
-             }
-         }
-     </style>
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
 
 
-     <!-- Custom styles for this template -->
+    <!-- Custom styles for this template -->
     <link href="form-validation.css" rel="stylesheet">
 </head>
+
 <body class="bg-light">
 
-<div class="container">
-    <main>
-        <div class="py-5 text-center">
+    <div class="container">
+        <main>
+            <div class="py-5 text-center">
 
-            <h2>Payement effectué</h2>
-            <p class="lead"> vous possédez maintenant une âme de moins</p>
+                <h2>Payement effectué</h2>
+                <p class="lead">Régalez-vous avec ces délicieuses sauces !</p>
+            </div>
+    </div>
+
+    <footer class="text-muted py-5">
+        <div class="container">
+            <iframe src="https://open.spotify.com/embed/track/2ChNJTAMVSDEc8hsdiF1Vs" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <p class="float-end mb-1">
+                <a href="../Shop" class="text-decoration-none">Retour à l'accueil
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"></path>
+                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"></path>
+                    </svg>
+                </a>
+            </p>
+            <p class="mb-0">Suivez-nous sur <a href="https://www.instagram.com/rawsauce_._/" target="_blank" class="text-decoration-none">Instagram
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Instagram.svg/1200px-Instagram.svg.png" width="17"></a></p>
+
         </div>
-
-
-</div>
-
-
-<footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">&copy; 2021 Sauce Co</p>
-    <ul class="list-inline">
-        <li class="list-inline-item"><a href="../Shop/index.php"> Home</a></li>
-    </ul>
-</footer>
-
-
-<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    </footer>
 </body>
-
 
 </html>
